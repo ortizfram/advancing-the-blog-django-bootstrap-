71bd28dda323f11420939b37b8e0d7030ce8a601
melon <ortizfranco48@gmail.com>
2023-09-14 18:18:39 -0300

    comment thread

--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Comment  # Import your Comment model
 
 def comment_thread(request, id):
-    # Retrieve comments based on the comment ID (adjust this based on your Comment model)
-    comments = Comment.objects.filter(parent_id=id)
-
-    # Pass comments to the template
-    return render(request, "comment_thread.html", {"comments": comments})
+    obj = get_object_or_404(Comment, id=id)
+    comments = Comment.objects.filter(parent=obj)  # Get child comments for the current comment
+    
+    # You may want to pass a comment form to your template if needed
+    # comment_form = YourCommentForm()  # Replace with your actual comment form
+    
+    context = {
+        "comment": obj,
+        "comments": comments,  # Pass child comments to the template
+        # "comment_form": comment_form,  # Include the comment form if needed
+    }
+    return render(request, "comment_thread.html", context)
