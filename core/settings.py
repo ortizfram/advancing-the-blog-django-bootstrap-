5ee4acd662650844381d4841db5aa7b0259fce40
melon <ortizfranco48@gmail.com>
2023-09-20 19:52:57 -0300

    render placeholder image static settings

--- core/settings.py
+++ core/settings.py
@@ -139,14 +139,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static_cdn/css/base.css/'
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static_cdn"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_cdn"),
+]
 
-# CDN => content delivey network
-STATIC_ROOT = os.path.join(BASE_DIR,"static_cdn")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR,"media_cdn")
 MEDIA_URL = "/media/"
