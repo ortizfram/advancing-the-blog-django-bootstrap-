eac618de0805dac249f4eae731f2d9a621d4bdb2
ortizfram <ortizfranco48@gmail.com>
2023-10-20 12:50:28 -0300

    CourseAdmin Video subclass

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 # ↓ after importing it in __init__ you can call as if it was in models.py
-from .models import Course, Learning, Prerequisite, Tag
+from .models import Course, Learning, Prerequisite, Tag, Video
 
 # Register your models here.
 
 # ☻ Create inline admin classes for related models...
 # ↓  These will be displayed within the CourseAdmin page.
-class TagAdmin(admin.TabularInline):
+class TagAdmin(admin.TabularInline):#shows admin in row style
     model = Tag
+
+class VideoAdmin(admin.TabularInline):# • StackedInline:shows admin in col style
+    model = Video
 
 class LearningAdmin(admin.TabularInline):
     model = Learning
@@ -19,7 +22,8 @@ class PrerequisiteAdmin(admin.TabularInline):
 # ↓  the admin interface for the Course model.
 class CourseAdmin(admin.ModelAdmin):
     # Include the inline admin classes for related models.
-    inlines = [TagAdmin, LearningAdmin, PrerequisiteAdmin]
+    inlines = [TagAdmin ,LearningAdmin, PrerequisiteAdmin, VideoAdmin]
 
 # ☻↓ Register the Course model with the custom admin interface.
 admin.site.register(Course, CourseAdmin)
+admin.site.register(Video)
