d6d290f85e88cbdd219da36f2bd041d8495cb614
ortizfram <ortizfranco48@gmail.com>
2023-10-20 11:04:25 -0300

    register model course

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Course # after importing it in __init__
 
 # Register your models here.
+
+admin.site.register(Course)
---
+++ apps/courses/models/__init__.py
@@ -0,0 +1,3 @@
+# importing my model from models/course/Course
+# makes it possible to register in admin panel
+from apps.courses.models.course import Course
