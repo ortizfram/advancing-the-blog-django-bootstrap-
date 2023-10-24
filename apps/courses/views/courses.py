532a6c642c93e9a853cd7c48894040dddedaf55b
ortizfram <ortizfranco48@gmail.com>
2023-10-24 12:35:00 -0300

    sorted videos

--- apps/courses/views/courses.py
+++ apps/courses/views/courses.py
@@ -11,6 +11,7 @@ def course_detail(request, slug):
     serial_number = request.GET.get('lecture')#from searchbar after click on lecture
     if serial_number is None : # for lecture [0]
         serial_number = 1
+    videos = course.video_set.all().order_by("serial_number")
     # ↓ return course and video lecture from video Model serial_number from Yt when uploading 
     # ↓ then ask it in frontend passing video_id to player and each video has it's {{video.serial_number}}">{{video}} 
     video = Video.objects.get(serial_number = serial_number, course = course)
@@ -18,5 +19,6 @@ def course_detail(request, slug):
     context = {
         'course' : course,
         'video': video,
+        'videos': videos,
     }
     return render(request, 'courses/course_detail.html', context=context)
